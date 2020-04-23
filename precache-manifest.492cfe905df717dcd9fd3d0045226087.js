8a9da7a6ac2fa901ff5e948bcae80b9dfeb218c8
sauravghosal <sghosal9@gatech.edu>
2020-04-23 16:16:54 -0400

    Updates

---
+++ precache-manifest.492cfe905df717dcd9fd3d0045226087.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538ba30a97be2db6313884a927df98ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c572836bd74878515822",
+    "url": "/static/css/2.f7ca946b.chunk.css"
+  },
+  {
+    "revision": "d4e997f92a625099f43f",
+    "url": "/static/css/main.36400d81.chunk.css"
+  },
+  {
+    "revision": "c572836bd74878515822",
+    "url": "/static/js/2.5ab3c879.chunk.js"
+  },
+  {
+    "revision": "39c826b8f3f4267b70b3d96ee16d9724",
+    "url": "/static/js/2.5ab3c879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e997f92a625099f43f",
+    "url": "/static/js/main.8804a839.chunk.js"
+  },
+  {
+    "revision": "a0311f6ad1c15d2d854b",
+    "url": "/static/js/runtime-main.12b590f4.js"
+  },
+  {
+    "revision": "47317a3ac237825452ef624d4e1af230",
+    "url": "/static/media/avatar.47317a3a.jpg"
+  },
+  {
+    "revision": "a9c5effcd236d54b7b2b58f3de9a2bd5",
+    "url": "/static/media/bgc-safety.a9c5effc.png"
+  },
+  {
+    "revision": "03c37427a06ea05a40663c96475ae862",
+    "url": "/static/media/biary-img.03c37427.png"
+  },
+  {
+    "revision": "98452aff937b8f68839c3db2e13821a4",
+    "url": "/static/media/funky-fridge-img.98452aff.png"
+  }
+]);
