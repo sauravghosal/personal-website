84ee14c746c21f213e30fe4fc972f76cbb4c966b
Saurav Ghosal <sghosal9@gatech.edu>
2021-12-30 01:56:44 -0500

    fixing circle absolute positioning

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Circles = ({
   top = ["auto", "auto"],
   bottom = ["auto", "auto"],
   right = ["auto", "auto"],
   left = ["auto", "auto"],
-}) => (
-  <div
-    className={`absolute top-${top[0]} bottom-${bottom[0]} right-${right[0]} left-${left[0]} md:top-${top[1]} md:top-${top[1]} md:bottom-${bottom[1]} md:right-${right[1]} md:left-${left[1]} z-0`}
-  >
-    <div className="relative z-0 w-80 h-80">
-      <div className="absolute top-0 -left-4 w-60 h-60 bg-blue-400 rounded-full opacity-10"></div>
-      <div className="absolute top-0 -right-4  w-60 h-60  bg-orange-500 rounded-full  opacity-10"></div>
-      <div className="absolute top-10 right-10 w-60 h-60  bg-green-500 rounded-full opacity-10"></div>
+}) => {
+  const mediaMatch = window.matchMedia("(min-width: 650px)");
+  const [matches, setMatches] = useState(mediaMatch.matches);
+
+  useEffect(() => {
+    const handler = (e) => setMatches(e.matches);
+    mediaMatch.addEventListener("change", handler);
+    return () => mediaMatch.removeEventListener("change", handler);
+  }, []);
+
+  const styles = (small) => {
+    return {
+      top: small ? top[1] : top[0],
+      bottom: small ? bottom[1] : bottom[0],
+      right: small ? right[1] : right[0],
+      left: small ? left[1] : left[0],
+    };
+  };
+
+  return (
+    <div className={`absolute`} style={styles(matches)}>
+      <div className="relative w-80 h-80">
+        <div className="absolute top-0 -left-4 w-60 h-60 bg-blue-400 rounded-full opacity-10"></div>
+        <div className="absolute top-0 -right-4  w-60 h-60  bg-orange-500 rounded-full  opacity-10"></div>
+        <div className="absolute top-10 right-10 w-60 h-60  bg-green-500 rounded-full opacity-10"></div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Circles;
