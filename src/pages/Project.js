39cbf71ac2bf8a50a4dff018ee604930497a4803
Saurav Ghosal <sghosal9@gatech.edu>
2021-12-30 01:09:44 -0500

    removing z index manipulation

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -16,7 +16,7 @@ const Project = () => {
   }, [id]);
 
   return (
-    <div className="z-10">
+    <div className="">
       <Link
         className="text-gray-400 flex items-center my-3 hover:cursor hover:text-gray-500"
         to="/projects"
@@ -27,7 +27,7 @@ const Project = () => {
       {Object.keys(project).length > 0 && (
         <div>
           <a href={project.link} target="_blank" rel="noopener noreferrer">
-            <h2 className="font-bold flex items-center hover:text-gray-800">
+            <h2 className="font-bold flex items-center hover:text-gray-800 my-2">
               {project.name} <BiLinkExternal className="mx-2" />
             </h2>
           </a>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,8 +7,8 @@ import Project from "./Project";
 const Projects = () => {
   const { path, url } = useRouteMatch();
   return (
-    <div className="min-h-screen my-5 z-10">
-      <div className="flex flex-col ">
+    <div className="min-h-screen my-5">
+      <div className="flex flex-col">
         <h2 className="mr-2 font-extrabold">Projects</h2>
 
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-2">
