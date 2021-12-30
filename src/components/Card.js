ab36fd18d9a8dc0afdd22ac41e583fc9abd93549
Saurav Ghosal <sghosal9@gatech.edu>
2021-12-30 00:32:15 -0500

    updating card component for project format changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { FaAward } from "react-icons/fa";
 
-const Card = ({ img, name, tags, content, award }) => {
+const Card = ({ img, name, tags, preview, award }) => {
   return (
-    <div className="no-underline flex flex-col md:flex-row rounded shadow-md transform hover:-translate-y-1 hover:scale-105 opacity-100 hover:bg-gray-100 hover:shadow-2xl duration-500 ease-in-out cursor-pointer">
+    <div className="h-full no-underline flex flex-col md:flex-row rounded shadow-md transform hover:-translate-y-1 hover:scale-105 opacity-100 hover:bg-gray-100 hover:shadow-2xl duration-500 ease-in-out cursor-pointer">
       {award && (
         <div className="text-red-500 ml-auto md:absolute right-0 mr-3">
           <FaAward className="inline-block" />
-          <p className="text-xs inline-block">HACKATHON WINNER</p>
+          <p className="text-xs inline-block">AWARD WINNER</p>
         </div>
       )}
       <img
@@ -18,7 +18,7 @@ const Card = ({ img, name, tags, content, award }) => {
       <div className="flex-grow">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{name}</div>
-          <p className="text-gray-700 text-base">{content} </p>
+          <p className="text-gray-700 text-base">{preview} </p>
         </div>
         <div className="px-6 pt-4 pb-2">
           {tags.map((tag) => {
