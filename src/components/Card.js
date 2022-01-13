9d2774213b7a4bd044544f94964ca7d0d638b845
Saurav Ghosal <sghosal9@gatech.edu>
2022-01-13 01:02:18 -0500

    styling project card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { FaAward } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const Card = ({ img, name, tags, preview, award }) => {
+const Card = ({ img, name, tags, preview, award, baseUrl, id }) => {
   return (
-    <div className="h-full no-underline flex flex-col md:flex-row rounded shadow-md transform hover:-translate-y-1 hover:scale-105 opacity-100 hover:bg-gray-100 hover:shadow-2xl duration-500 ease-in-out cursor-pointer">
+    <Link
+      to={`${baseUrl}/${id}`}
+      className="flex flex-col sm:flex-row w-full sm:h-60 border-gray-200 border bg-white m-2 no-underline rounded shadow-md transform hover:scale-105 hover:opacity-100 hover:shadow-2xl duration-500 ease-in-out cursor-pointer"
+    >
       {award && (
-        <div className="text-red-500 ml-auto md:absolute right-0 mr-3">
+        <div className="text-red-500 ml-auto absolute right-0 mr-3">
           <FaAward className="inline-block" />
           <p className="text-xs inline-block">AWARD WINNER</p>
         </div>
       )}
-      <img
-        src={img}
-        className="p-2 w-full md:w-60 lg:w-52 xl:w-60 object-contain"
-        alt="project graphic"
-      />
+      <img src={img} className="p-2 w-full sm:w-60" alt="project graphic" />
       <div className="flex-grow">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{name}</div>
@@ -30,7 +30,7 @@ const Card = ({ img, name, tags, preview, award }) => {
           })}
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,30 +1,29 @@
 import React from "react";
 import Card from "../components/Card";
 import { projects } from "../constants";
-import { Route, useRouteMatch, Link } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import Project from "./Project";
 
 const Projects = () => {
   const { path, url } = useRouteMatch();
   return (
-    <div className="min-h-screen my-5" style={{ zIndex: 5 }}>
+    <div className="min-h-screen my-5">
       <div className="flex flex-col">
         <h2 className="mr-2 font-extrabold">Projects</h2>
 
-        <div className="grid grid-cols-1 xl:grid-cols-2 gap-2">
+        <div className="flex flex-wrap">
           {projects.map(({ name, preview, link, img, award, tags, id }) => {
             return (
-              <Link to={`${url}/${id}`}>
-                <Card
-                  name={name}
-                  preview={preview}
-                  link={link}
-                  img={img}
-                  award={award}
-                  tags={tags}
-                  id={id}
-                />
-              </Link>
+              <Card
+                name={name}
+                preview={preview}
+                link={link}
+                baseUrl={url}
+                img={img}
+                award={award}
+                tags={tags}
+                id={id}
+              />
             );
           })}
         </div>
